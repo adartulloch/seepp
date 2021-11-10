bdcf44acf933a40e4769b83ed78a99710f21f257
VishTiwari <60826496+VishTiwari@users.noreply.github.com>
2021-11-10 16:11:45 -0500

    Added Sast
    sast

---
+++ sast.ml
@@ -0,0 +1,32 @@
+open Ast
+
+type sexpr =
+  SLiteral of int * typ
+  | SStringLit of string * typ
+  | SBoolLit of bool * typ
+  | SPixelLit of sexpr * sexpr * sexpr * sexpr * typ
+  | SId of string * typ
+  | SBinop of sexpr * op * sexpr * typ
+  | SUnop of uop * sexpr * typ
+  | SAssign of string * sexpr * typ
+  | SAssignp of string * field * sexpr * typ
+  | SAssignm of string * sexpr * sexpr * sexpr * typ
+  | SCall of string * sexpr list * typ
+  | SAccess of string * field * typ
+  | SNoexpr
+
+type sstmt =
+    SBlock of sstmt list
+  | SExpr of sexpr * typ
+  | SReturn of sexpr
+  | SIf of sexpr * sstmt * sstmt
+  | SFor of sexpr * sexpr * sexpr * sstmt
+  | SWhile of sexpr * sstmt
+
+type sfunc_decl = {
+    styp : typ;
+    sfname : string;
+    sformals : bind list;
+    slocals : bind list;
+    sbody : sstmt list;
+}
