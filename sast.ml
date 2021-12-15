cd6e1470f264ae2d57cfba1bcb0324a2b8926449
jrl2218 <93962058+jrl2218@users.noreply.github.com>
2021-12-14 23:04:44 -0500

    commiting small changes to sast.

--- sast.ml
+++ sast.ml
@@ -1,5 +1,6 @@
 (* Semantically-checked Abstract Syntax Tree and functions for printing it *)
 
+
 open Ast
 
 type sexpr = typ * sx
@@ -7,15 +8,20 @@ and sx =
     SLiteral of int
   | SFliteral of string
   | SBoolLit of bool
+  | SCharLit of char
+  | SStringLit of string
   | SId of string
   | SBinop of sexpr * op * sexpr
+  | SField of string * sexpr
   | SUnop of uop * sexpr
   | SAssign of string * sexpr
   | SCall of string * sexpr list
   | SNoexpr
 
 type sstmt =
-    SBlock of sstmt list
+    SVDecl of typ * string
+  | SVDeclAssign of typ * string * sexpr
+  | SBlock of sstmt list
   | SExpr of sexpr
   | SReturn of sexpr
   | SIf of sexpr * sstmt * sstmt
@@ -26,7 +32,6 @@ type sfunc_decl = {
     styp : typ;
     sfname : string;
     sformals : bind list;
-    slocals : bind list;
     sbody : sstmt list;
   }
 
@@ -37,9 +42,12 @@ type sprogram = bind list * sfunc_decl list
 let rec string_of_sexpr (t, e) =
   "(" ^ string_of_typ t ^ " : " ^ (match e with
     SLiteral(l) -> string_of_int l
+  | SFliteral(l) -> l
   | SBoolLit(true) -> "true"
   | SBoolLit(false) -> "false"
-  | SFliteral(l) -> l
+  | SCharLit(l) -> String.make 1 l
+  | SStringLit(l) -> l
+  | SField(e,f) -> e ^ "." ^ string_of_sexpr f
   | SId(s) -> s
   | SBinop(e1, o, e2) ->
       string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
@@ -47,11 +55,12 @@ let rec string_of_sexpr (t, e) =
   | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
   | SCall(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
-  | SNoexpr -> ""
-				  ) ^ ")"				     
+  | SNoexpr -> ""  ) ^ ")"     
 
 let rec string_of_sstmt = function
-    SBlock(stmts) ->
+    SVDecl(t, i) -> string_of_typ t ^ " " ^ i ^ "\n"
+  | SVDeclAssign(t, i, e) -> string_of_typ t ^ " " ^ i ^ " = " ^ string_of_sexpr e ^ "\n"
+  | SBlock(stmts) ->
       "{\n" ^ String.concat "" (List.map string_of_sstmt stmts) ^ "}\n"
   | SExpr(expr) -> string_of_sexpr expr ^ ";\n";
   | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ ";\n";
@@ -68,7 +77,7 @@ let string_of_sfdecl fdecl =
   string_of_typ fdecl.styp ^ " " ^
   fdecl.sfname ^ "(" ^ String.concat ", " (List.map snd fdecl.sformals) ^
   ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.slocals) ^
+  (* String.concat "" (List.map string_of_vdecl fdecl.slocals) ^ *)
   String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
   "}\n"
 
